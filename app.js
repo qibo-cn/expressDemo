cfe95a83f27650c71d79344710123c1cd8554e26
Qi bo <qibo_cn@outlook.com>
2021-01-17 14:55:39 +0800

    dym html

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require("express");
 var app = express();
+// 加载hbs模块
+var hbs = require("hbs");
+// 指定模板文件的后缀名为html
+app.set("view engine", "html");
 
-app.get("/index", (req, response) => {
-    response.sendFile(__dirname + "/views/index.html");
-});
-app.get("/about", (req, response) => {
-    response.sendFile(__dirname + "/views/about.html");
+// 运行hbs模块
+app.engine("html", hbs.__express);
+
+app.get("/", function (req, res) {
+    res.render("index");
 });
 
-app.get("/article", (req, response) => {
-    response.sendFile(__dirname + "/views/article.html");
+app.get("/about", function (req, res) {
+    res.render("about");
+});
+
+app.get("/article", function (req, res) {
+    res.render("article");
 });
 
 app.listen(3000);
